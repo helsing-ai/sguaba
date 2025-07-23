1beb22a42ce1d9309e76be95ac418b3762ea07b1
Alexander Gessler <alexander.gessler@helsing.ai>
2025-07-23 19:25:03 +0200

    directions: small updates to Bearing docs
    I do consider this one a minor foot-gun: while mathematically correct, many users will be working with or expecting normalized (or normalized signed) angle representations. LMK what you think.

--- src/directions.rs
+++ src/directions.rs
@@ -146,6 +146,8 @@ impl<In> Bearing<In> {
     ///
     /// The definition of the azimuthal angle depends on the implementation of [`BearingDefined`]
     /// for `In`.
+    ///
+    /// Note: There is no guarantee on the numeric range of the azimuthal angle.
     #[must_use]
     pub fn azimuth(&self) -> Angle {
         self.azimuth
@@ -156,7 +158,7 @@ impl<In> Bearing<In> {
     /// The definition of the azimuthal angle depends on the implementation of [`BearingDefined`]
     /// for `In`.
     ///
-    /// The returned value is always in [-90°, 90°].
+    /// The returned value is always in [-90°, 90°] % 360°
     #[must_use]
     pub fn elevation(&self) -> Angle {
         self.elevation
