0a83be5f8ccebe064c19c3668179cb9d11f75818
Daniel Albert <github@esclear.de>
2026-02-16 19:18:57 +0100

    use tait bryan builder

--- src/math.rs
+++ src/math.rs
@@ -3045,8 +3045,13 @@ mod tests {
         let q_rotation = unsafe { Rotation::<Ecef, Ecef>::from_quaternion(q.0, q.1, q.2, q.3) };
         // Negate the angles, as the rotation is a rotation _of_ the transformed vector _in_ the
         // rotated system, see the `from_tait_bryan_angles` documentation for details.
-        let ypr_rotation =
-            unsafe { Rotation::<Ecef, Ecef>::from_tait_bryan_angles(-ypr.0, -ypr.1, -ypr.2) };
+        let ypr_rotation = unsafe {
+            Rotation::<Ecef, Ecef>::tait_bryan_builder()
+                .yaw(-ypr.0)
+                .pitch(-ypr.1)
+                .roll(-ypr.2)
+                .build()
+        };
 
         let x = coordinate!(x = m(1.0), y = m(0.0), z = m(0.0); in Ecef);
         let y = coordinate!(x = m(0.0), y = m(1.0), z = m(0.0); in Ecef);
